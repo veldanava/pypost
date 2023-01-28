f10fd079412e4c287094dac933961fed937b59dd
urmooddeveloper <86060881+veldanava@users.noreply.github.com>
2023-01-28 21:13:03 +0700

    Update post.py

--- post.py
+++ post.py
@@ -1,7 +1,10 @@
 # py post for ctf
 import requests as req
+
+# url or host
 url = "http://165.227.106.113/post.php"
-data = {"username":"<censored>","password":"<censored>"}
+data = {"username":"username","password":"password"}
 res = req.post(url , data=data)
+
 print(res.status_code)
 print(res.text)
